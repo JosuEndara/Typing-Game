823a08c0401d8f89471f55c4a1bb7c50a8414dca
JosuEndara <sasdexl@gmail.com>
2023-07-17 00:01:24 -0500

    Typing game finished

---
+++ script.js
@@ -0,0 +1,86 @@
+const quotes = [
+    'Si el amor es una droga yo quiero ser el narcotraficante.',
+    'Si tu me odias, quedare yo convencido',
+    'Odiame por piedad yo te lo pido',
+    'Se burlaran de mi que nadie sepa mi sufrir',
+    'No te apartes del camino bella luz que me ilumina',
+];
+
+// array for storing the words of the current challenge
+let words = [];
+// stores the index of the word the player is currently typing
+let wordIndex = 0;
+// default value for startTime (will be set on start)
+let startTime = Date.now();
+
+// grab UI items
+const quoteElement = document.getElementById('quote');
+const messageElement = document.getElementById('message')
+const typedValueElement = document.getElementById('typed-value');
+
+document.getElementById('start').addEventListener('click', function () {
+    // get a quote
+    const quoteIndex = Math.floor(Math.random() * quotes.length);
+    const quote = quotes[quoteIndex];
+    // Put the quote into an array of words
+    words = quote.split(' ');
+    // reset the word index for tracking
+    wordIndex = 0;
+
+    // UI updates
+    //span is generic container
+    //map pass for each element and execute a function en each item
+    //it means that it takes the word and wrap into the span container
+    // Create an array of span elements so we can set a class
+    const spanWords = words.map(function (word) { return `<span>${word} </span>` });
+    // Convert into string and set as innerHTML on quote display
+    quoteElement.innerHTML = spanWords.join('');
+    // Highlight the first word
+    quoteElement.childNodes[0].className = 'highlight';
+    // Clear any prior messages
+    messageElement.innerText = '';
+
+    // Setup the textbox
+    // Clear the textbox
+    typedValueElement.value = '';
+    // set focus
+    typedValueElement.focus();
+    // set the event handler
+
+    // Start the timer
+    startTime = new Date().getTime();
+});
+
+typedValueElement.addEventListener('input', (e) => {
+    // Get the current word
+    const currentWord = words[wordIndex];
+    // get the current value
+    const typedValue = typedValueElement.value;
+
+    if (typedValue === currentWord && wordIndex === words.length - 1) {
+        // end of quote
+        // Display success
+        const elapsedTime = new Date().getTime() - startTime;
+        const message = `CONGRATULATIONS! You finished in ${elapsedTime / 1000} seconds.`;
+        messageElement.innerText = message;
+    } else if (typedValue.endsWith(' ') && typedValue.trim() === currentWord) {
+        // end of word
+        // clear the typedValueElement for the new word
+        typedValueElement.value = '';
+        // move to the next word
+        wordIndex++;
+        // reset the class name for all elements in quote
+        for (const wordElement of quoteElement.childNodes) {
+            wordElement.className = '';
+        }
+        // highlight the new word
+        quoteElement.childNodes[wordIndex].className = 'highlight';
+    } else if (currentWord.startsWith(typedValue)) {
+        // currently correct
+        // highlight the next word
+        typedValueElement.className = '';
+    } else {
+        // error state
+        typedValueElement.className = 'error';
+    }
+});
